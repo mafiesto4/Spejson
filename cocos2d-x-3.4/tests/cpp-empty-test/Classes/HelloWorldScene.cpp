417fc793895c0336b848c89d2adc4548e47c31c6
mafiesto4 <mafiesto4@wp.pl>
2015-03-22 20:45:19 +0100

    Reszta plików

--- cocos2d-x-3.4/tests/cpp-empty-test/Classes/HelloWorldScene.cpp
+++ cocos2d-x-3.4/tests/cpp-empty-test/Classes/HelloWorldScene.cpp
@@ -6,86 +6,86 @@ USING_NS_CC;
 
 Scene* HelloWorld::scene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    HelloWorld *layer = HelloWorld::create();
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+	
+	// 'layer' is an autorelease object
+	HelloWorld *layer = HelloWorld::create();
 
-    // add layer as a child to scene
-    scene->addChild(layer);
+	// add layer as a child to scene
+	scene->addChild(layer);
 
-    // return the scene
-    return scene;
+	// return the scene
+	return scene;
 }
 
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
-    
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    auto origin = Director::getInstance()->getVisibleOrigin();
+	//////////////////////////////
+	// 1. super init first
+	if ( !Layer::init() )
+	{
+		return false;
+	}
+	
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	auto origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+	/////////////////////////////
+	// 2. add a menu item with "X" image, which is clicked to quit the program
+	//    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                        "../Resources/Textures/CloseNormal.png",
-                                        "../Resources/Textures/CloseSelected.png",
-                                        CC_CALLBACK_1(HelloWorld::menuCloseCallback,this));
-    
-    closeItem->setPosition(origin + Vec2(visibleSize) - Vec2(closeItem->getContentSize() / 2));
+	// add a "close" icon to exit the progress. it's an autorelease object
+	auto closeItem = MenuItemImage::create(
+										"../Resources/Textures/CloseNormal.png",
+										"../Resources/Textures/CloseSelected.png",
+										CC_CALLBACK_1(HelloWorld::menuCloseCallback,this));
+	
+	closeItem->setPosition(origin + Vec2(visibleSize) - Vec2(closeItem->getContentSize() / 2));
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, nullptr);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-    
-    /////////////////////////////
-    // 3. add your codes below...
+	// create menu, it's an autorelease object
+	auto menu = Menu::create(closeItem, nullptr);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 1);
+	
+	/////////////////////////////
+	// 3. add your codes below...
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = LabelTTF::create("Trolololo121", "Arial", TITLE_FONT_SIZE);
-    
-    // position the label on the center of the screen
-    label->setPosition(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height);
+	// add a label shows "Hello World"
+	// create and initialize a label
+	
+	auto label = LabelTTF::create("Trolololo121", "Arial", TITLE_FONT_SIZE);
+	
+	// position the label on the center of the screen
+	label->setPosition(origin.x + visibleSize.width/2,
+							origin.y + visibleSize.height - label->getContentSize().height);
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
+	// add the label as a child to this layer
+	this->addChild(label, 1);
 
-    // add "HelloWorld" splash screen"
+	// add "HelloWorld" splash screen"
 	auto sprite = Sprite::create("../Resources/Textures/HelloWorld.png");
 
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize / 2) + origin);
+	// position the sprite on the center of the screen
+	sprite->setPosition(Vec2(visibleSize / 2) + origin);
 
-    // add the sprite as a child to this layer
-    this->addChild(sprite);
-    
-    return true;
+	// add the sprite as a child to this layer
+	this->addChild(sprite);
+	
+	return true;
 }
 
 void HelloWorld::menuCloseCallback(Ref* sender)
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
 	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-    return;
+	return;
 #endif
 
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+	exit(0);
 #endif
 }
